3813656f63d3ae3c6e0df9b2d906283cc1d81bbc
Amaury Trujillo <2842742+amauryt@users.noreply.github.com>
2024-05-27 23:20:53 +0200

    Refactor HugeIntHelper.i128_to_huge

--- src/duckdb/huge_int_helper.cr
+++ src/duckdb/huge_int_helper.cr
@@ -12,17 +12,11 @@ module DuckDB
 
     def self.i128_to_huge(value : Int128) : LibDuckDB::HugeInt
       aux_io = IO::Memory.new(16)
-      lower_io = IO::Memory.new(8)
-      upper_io = IO::Memory.new(8)
       value.to_io(aux_io, FORMAT)
-      aux_bytes = aux_io.to_slice
-      upper_io.write(aux_bytes[0..7])
-      lower_io.write(aux_bytes[8..15])
-      upper_io.rewind
-      lower_io.rewind
+      aux_io.rewind
       result = LibDuckDB::HugeInt.new
-      result.upper = Int64.from_io(upper_io, FORMAT)
-      result.lower = UInt64.from_io(lower_io, FORMAT)
+      result.upper = Int64.from_io(aux_io, FORMAT)
+      result.lower = UInt64.from_io(aux_io, FORMAT)
       result
     end
   end
